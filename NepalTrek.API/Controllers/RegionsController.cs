dbf03e2db45de091f0e0f7656f63508d11d6f4d4
anish0512 <maharzn11@gmail.com>
2024-03-13 13:17:00 +0545

    Added Region Controller

---
+++ NepalTrek.API/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NepalTrek.API.Models.Domain;
+
+namespace NepalTrek.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region> {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name="Annapurna Region",
+                    Code="ABC",
+                    RegionImageUrl="https://www.marveladventure.com/uploads/editors/Annapurna-Base-Camp-Trek-in-January-and-February-1.jpg"
+                },
+                 new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name="Langtang Region",
+                    Code="LAN",
+                    RegionImageUrl="https://www.magicalsummits.com/wp-content/uploads/2023/02/Langtang-Valley-Trek-1.jpg"
+                },
+            };
+
+            return Ok(regions);
+        }
+    }
+}
