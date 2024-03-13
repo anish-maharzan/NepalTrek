95f3b6649e7e704613f38fc4e621dd268c67707b
anish0512 <maharzn11@gmail.com>
2024-03-13 13:04:50 +0545

    Added DbContext Class and DbSet properties -Added DI for DbContext in program.cs -Added ConnectionString in appsetting.json -Run Add-Migration & Update-Database

---
+++ NepalTrek.API/Data/NepalTrekDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NepalTrek.API.Models.Domain;
+
+namespace NepalTrek.API.Data
+{
+    public class NepalTrekDbContext:DbContext
+    {
+        public NepalTrekDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
